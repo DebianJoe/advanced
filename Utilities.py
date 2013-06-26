52a9b7dbc6063d2f9dfffff3d765bc91494d5262
Wesley Werner <wez@darknet.co.za>
2013-06-26 23:03:47 +0200

    add helpers to get line segments, and check for line of sight

--- Utilities.py
+++ Utilities.py
@@ -112,6 +112,73 @@ def distanceBetweenPoints(x, y, u, v):
     dy = y - v
     return math.sqrt(dx ** 2 + dy ** 2)
 
+def make_matrix(width, height, initial_value):
+    """
+    Returns a list of initial values that can be accessed like a 2D array:
+
+        matrix[x][y]
+
+    """
+
+    return [[initial_value for y in range(0, height)] for x in range(0, width)]
+
+def get_line_segments(x1, y1, x2, y2):
+    """
+    Returns a list of line segments that make up a line between two points.
+    Returns [(x1, y1), (x2, y2), ...]
+
+    Source: http://roguebasin.roguelikedevelopment.org/index.php?title=Bresenham%27s_Line_Algorithm
+
+    """
+
+    points = []
+    issteep = abs(y2 - y1) > abs(x2 - x1)
+    if issteep:
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+    rev = False
+    if x1 > x2:
+        x1, x2 = x2, x1
+        y1, y2 = y2, y1
+        rev = True
+    deltax = x2 - x1
+    deltay = abs(y2 - y1)
+    error = int(deltax / 2)
+    y = y1
+    ystep = None
+    if y1 < y2:
+        ystep = 1
+    else:
+        ystep = -1
+    for x in range(x1, x2 + 1):
+        if issteep:
+            points.append((y, x))
+        else:
+            points.append((x, y))
+        error -= deltay
+        if error < 0:
+            y += ystep
+            error += deltax
+    # Reverse the list if the coordinates were reversed
+    if rev:
+        points.reverse()
+    return points
+
+def line_of_sight(matrix, x1, y1, x2, y2):
+    """
+    Returns True if there is line of sight between two points.
+    Uses the matrix data to rely on blocking tiles.
+    This is a matrix created with make_matrix().
+    matrix values of 0 or False are not solid, 1 or True are solid.
+
+    """
+
+    segs = get_line_segments(x1, y1, x2, y2)
+    hits = [matrix[x][y] for x, y in segs]
+    amt = hits.count(True)
+    # allow 1 case: if the final destination position is blocking
+    return amt == 0 or (amt == 1 and matrix[x2][y2])
+
 
 class GameError(Exception):
     """
